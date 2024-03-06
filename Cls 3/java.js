84c46296fb39e3f4ab56fb1182c10c5f7ba61231
WendyTatEcheverri <159964810+WendyTatEcheverri@users.noreply.github.com>
2024-03-06 08:53:07 -0500

    agrergclase3

---
+++ Cls 3/java.js
@@ -0,0 +1,31 @@
+let nombreUsuario;
+let user ="Wendy";
+console.log(nombreUsuario);
+console.log(user);
+
+user === "Pedro"?console.log("verdadero"):console.log("Falso");
+
+const PI=3.1416;
+
+//Metodos propios del lenguaje
+let PI_DOS;
+PI_DOS=Math.PI;
+user.includes("P")
+console.log(user.length);
+
+//metodos o funciones
+const saludarusuario=()=>{
+    return "Hola Wendy";
+}
+const saludarUser = nameUser=>{
+    return "Hola" + nameUser;
+}
+
+const sumarDatos=(numero1, numero2) =>{
+    return numero1+numero2
+}
+
+console.log(sumarDatos(1.0,'2'));
+
+let tres =saludarUser;
+console.log(tres("Pedro"));
